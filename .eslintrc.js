172091c956abe2fd95c70c80e14a95a11a6ac049
miiton <vo.gu.ba.miiton@gmail.com>
2017-06-30 16:44:55 +0900

    👍 prettier前提

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   // So parent files don't get applied
   root: true,
+  parser: 'babel-eslint',
   extends: 'standard',
-  plugins: [
-    'react',
-    'import',
-  ],
+  plugins: ['react', 'import'],
+  env: {
+    browser: true,
+  },
   rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': 'off',
+    'generator-star-spacing': ['error', { before: false, after: true }],
     // React
     'react/display-name': 'error',
     'react/jsx-boolean-value': ['error', 'always'],
@@ -15,17 +19,20 @@ module.exports = {
     'react/jsx-equals-spacing': 'error',
     'react/jsx-first-prop-new-line': ['error', 'multiline'],
     'react/jsx-handler-names': 'error',
-    'react/jsx-indent-props': ['error', 2],
-    'react/jsx-max-props-per-line': ['error', {
-      maximum: 3
-    }],
+    'react/jsx-indent-props': 'off',
+    'react/jsx-max-props-per-line': [
+      'error',
+      {
+        maximum: 3,
+      },
+    ],
     'react/jsx-no-duplicate-props': 'error',
     'react/jsx-no-undef': 'error',
     'react/jsx-pascal-case': 'error',
     'react/jsx-space-before-closing': 'error',
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
-    'react/no-danger': 'error',
+    'react/no-danger': 'off',
     'react/no-did-mount-set-state': 'error',
     'react/no-did-update-set-state': 'error',
     'react/no-direct-mutation-state': 'error',
@@ -41,7 +48,7 @@ module.exports = {
     'react/sort-comp': 'error',
     'react/sort-prop-types': 'error',
     'react/jsx-wrap-multilines': 'error',
-    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent': 'off',
 
     // React Disabled
     'react/jsx-no-bind': 'off',
@@ -49,4 +56,7 @@ module.exports = {
     'react/jsx-sort-props': 'off',
     'react/no-set-state': 'off',
   },
-};
+  globals: {
+    Generator: true,
+  },
+}
